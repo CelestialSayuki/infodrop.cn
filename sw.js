511dbf7cc68a2b6ccb3a261e9a4b5167b8559415
Raymond Zhang <46773007@qq.com>
2025-08-09 12:47:04 +0800

    Fix animation.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1A1011z';
+const CACHE_VERSION = '1A1012v';
 const CACHE_NAME = `project-mammoth-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `project-mammoth-runtime-${CACHE_VERSION}`;
 const APP_SHELL_URL = './index.html';
