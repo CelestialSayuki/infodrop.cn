cd03942e7bd1ba0b07aaca713eacd136516ad9f3
Raymond Zhang <46773007@qq.com>
2025-08-17 17:32:46 +0800

    add glow border

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1A1015y';
+const CACHE_VERSION = '1A1016y';
 const CACHE_NAME = `project-mammoth-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `project-mammoth-runtime-${CACHE_VERSION}`;
 const APP_SHELL_URL = './index.html';
