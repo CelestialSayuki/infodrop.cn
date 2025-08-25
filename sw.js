b698580613410698c20ed30860cc7d8324a29567
Raymond Zhang <46773007@qq.com>
2025-08-25 13:01:37 +0800

    grouping

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1A1024z';
+const CACHE_VERSION = '1A1025r';
 const CACHE_NAME = `project-mammoth-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `project-mammoth-runtime-${CACHE_VERSION}`;
 const APP_SHELL_URL = './index.html';
