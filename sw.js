4d9c8a337bbafa9999f88c6629e83a68144ac788
Raymond Zhang <46773007@qq.com>
2025-09-04 23:41:35 +0800

    Fix css issue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1A1054b';
+const CACHE_VERSION = '1A1054c';
 const CACHE_NAME = `project-mammoth-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `project-mammoth-runtime-${CACHE_VERSION}`;
 const APP_SHELL_URL = './index.html';
