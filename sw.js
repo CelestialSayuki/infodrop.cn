85ecf62e1604f7ab68fed5748abe9e1053201c56
Raymond Zhang <46773007@qq.com>
2025-09-16 20:30:11 +0800

    Fix typo.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1A0720001a';
+const CACHE_VERSION = '1A0720002a';
 const CACHE_NAME = `project-mammoth-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `project-mammoth-runtime-${CACHE_VERSION}`;
 const APP_SHELL_URL = './index.html';
