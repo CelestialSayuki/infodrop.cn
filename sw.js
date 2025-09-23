1eba0bf669d78a027c6eb96e73f46584d37509df
CelestialSayuki <46773007@qq.com>
2025-09-24 00:15:51 +0800

    fix a bug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1A076 (d)';
+const CACHE_VERSION = '1A077';
 const CACHE_NAME = `project-mammoth-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `project-mammoth-runtime-${CACHE_VERSION}`;
 const MANIFEST_URL = './precache-manifest.json';
