148103885f33b1414954535db9eec8f4631481f2
CelestialSayuki <46773007@qq.com>
2025-11-04 14:37:19 +0800

    Add minecraft server.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'temp-rev4';
+const CACHE_VERSION = 'temp-rev5';
 const CACHE_NAME = `project-mammoth-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `project-mammoth-runtime-${CACHE_VERSION}`;
 const MANIFEST_URL = './precache-manifest.json';
