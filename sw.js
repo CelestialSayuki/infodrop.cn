febbef97f82a4d734850e51ac1c81c3db5a3960e
CelestialSayuki <46773007@qq.com>
2026-02-06 17:06:52 +0800

    Update GB6 Score

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2A1007d';
+const CACHE_VERSION = '2A1008d';
 const CACHE_NAME = `infodrop-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE_NAME = `infodrop-runtime-${CACHE_VERSION}`;
 const MANIFEST_URL = './precache-manifest.json';
